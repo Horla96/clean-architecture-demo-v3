71dbda1447da057efb44fe97f7947fdaebadba5f
Horla96 <sexysulai@gmail.com>
2025-12-02 01:36:35 +0100

    Setup EF Core and Add Product Entity Configuration

--- Persistance/ServiceExtensions.cs
+++ Persistance/ServiceExtensions.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
 
 namespace Persistence
 {
     public static class ServiceExtensions
     {
-        public static void AddPersistence(this IServiceCollection services)
+        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
-
+            // register services
+            services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection")
+                ));
         }
     }
 }
