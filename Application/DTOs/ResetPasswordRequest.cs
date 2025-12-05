f295a4e428789e6f8d03c6796d994e8420fd6571
Horla96 <sexysulai@gmail.com>
2025-12-05 13:25:52 +0100

    Implement Forgot Password and Reset Password Functionality

---
+++ Application/DTOs/ResetPasswordRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs
+{
+    public class ResetPasswordRequest
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
